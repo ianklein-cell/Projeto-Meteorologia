eefdabe60fcd5398d8989214dbc335e7da6ee7ae
Luizito07 <luizfilipeflores2020@gmail.com>
2026-09-16 09:29:18 -0300

    Corrige conflitos e atualiza projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Brasil from "./pages/Brasil";
 import Japao from "./pages/Japao";
 import EUA from "./pages/EUA";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/brasil" element={<Brasil />}></Route>
-        <Route path="/japao" element={<Japao />}></Route>
-        <Route path="/eua" element={<EUA />}></Route>
+        <Route path="/brasil" element={<Brasil />} />
+        <Route path="/japao" element={<Japao />} />
+        <Route path="/eua" element={<EUA />} />
       </Routes>
     </BrowserRouter>
   );
-=======
-export default function App() {
-  return (
-    <div>App</div>
-  )
->>>>>>> 50b422dc93cb7670f389bcbf75b38efa75de2a1a
 }
