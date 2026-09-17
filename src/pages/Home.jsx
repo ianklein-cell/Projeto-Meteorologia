a5beb411d10c1ad95864651ee675cbbfc89dc96c
ianklein-cell <ian.klein@aluno.senai.br>
2026-09-17 10:53:26 -0300

    Att fases da lua

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,16 @@
 import { Link } from "react-router-dom";
-import Header from "../componets/Header";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <h1>Previsão do Tempo Mundial 🌍</h1>
-        <p>Selecione um país para consultar o clima:</p>
-        <div>
-          <Link to="/brasil">🇧🇷 Brasil</Link>
-          <Link to="/japao">🇯🇵 Japão</Link>
-          <Link to="/eua">🇺🇸 EUA</Link>
-        </div>
-      </main>
-    </>
+    <div>
+      <h1>Previsão do Tempo Mundial 🌍</h1>
+      <p>Selecione um país para consultar o clima:</p>
+
+      <div>
+        <Link to="/brasil">🇧🇷 Brasil</Link>
+        <Link to="/japao">🇯🇵 Japão</Link>
+        <Link to="/eua">🇺🇸 EUA</Link>
+      </div>
+    </div>
   );
 }
